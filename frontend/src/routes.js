76c1bcca17f5d67428f4eaa85f311b6c39b162cb
BarisYildirim6 <byildirim2561@gmail.com>
2023-03-20 02:30:02 +0300

    Frontend base implemented

---
+++ frontend/src/routes.js
@@ -0,0 +1,14 @@
+import Splash from './pages/Splash';
+
+const routes = [
+	{
+		type: 'collapse',
+		name: 'Splash',
+		key: 'splash',
+		route: '/splash',
+		label: 'Splash',
+		component: <Splash />,
+	},
+];
+
+export default routes;
