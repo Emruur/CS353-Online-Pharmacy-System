68c0ac654d3d64f7323e92d0f55014e8ea96d320
BarisYildirim6 <byildirim2561@gmail.com>
2023-05-24 00:56:23 +0300

    CORS configuration implemented

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from users import users_blueprint
 from auth import auth_blueprint
 from prescription import prescription_blueprint
@@ -7,6 +8,8 @@ from flask_jwt_extended import JWTManager
 
 # Configuration for Flask-JWT-Extended
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'     # CORS header
 app.config['JWT_SECRET_KEY'] = 'cs353pharmacy'  # replace 'your-secret-key' with your actual secret key
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 86400  # the number of seconds until the token expires, you can adjust this as needed
 
