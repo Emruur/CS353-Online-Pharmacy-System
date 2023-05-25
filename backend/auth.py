1efdad42e8a426af82329be40df27d853d7b239e
Emre Uğur <46279069+Emruur@users.noreply.github.com>
2023-05-25 13:08:38 +0300

    fixed signup

--- backend/auth.py
+++ backend/auth.py
@@ -53,7 +53,6 @@ def login():
     access_token = create_access_token(identity=user['user_id'], additional_claims=additional_info)
     return jsonify(access_token=access_token), 200
 
-
 @auth_blueprint.route('/signup', methods=['POST'])
 def signup():
     if not request.is_json:
@@ -68,17 +67,45 @@ def signup():
     surname = request.json.get('surname', None)
     phone_number = request.json.get('phone_number', None)
     user_type = request.json.get('user_type', None)
+    type_specific = request.json.get('type_specific', {})
+
+    if not email or not user_id or not password or not first_name or not surname or not phone_number or not user_type:
+        return jsonify({"msg": "Missing required parameters"}), 400
 
     # hash the password using bcrypt
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
     conn = get_conn()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO User (user_id,email, password, first_name, middle_name, surname, phone_number, user_type) VALUES (%s,%s, %s, %s, %s, %s, %s, %s)",
-                   (user_id,email, hashed_password, first_name, middle_name, surname, phone_number, user_type))
+    
+    # check if email, user_id or phone_number is already in use
+    cursor.execute("SELECT email, user_id, phone_number FROM User WHERE email = %s OR user_id = %s OR phone_number = %s", (email, user_id, phone_number))
+    existing_user = cursor.fetchone()
+    if existing_user:
+        return jsonify({"msg": "Email, User ID or Phone Number already in use"}), 400
+
+    # insert new user
+    cursor.execute("INSERT INTO User (user_id, email, password, first_name, middle_name, surname, phone_number, user_type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                   (user_id, email, hashed_password, first_name, middle_name, surname, phone_number, user_type))
+    conn.commit()
+
+    # insert into the respective user_type table
+    if user_type == 'patient':
+        cursor.execute("INSERT INTO Patient (user_id, height, weight) VALUES (%s, %s, %s)",
+                       (user_id, type_specific.get('height'), type_specific.get('weight')))
+    elif user_type == 'doctor':
+        cursor.execute("INSERT INTO Doctor (user_id, speciality) VALUES (%s, %s)",
+                       (user_id, type_specific.get('speciality')))
+    elif user_type == 'pharmacist':
+        cursor.execute("INSERT INTO Pharmacist (user_id, education) VALUES (%s, %s)",
+                       (user_id, type_specific.get('education')))
+    else:
+        return jsonify({"msg": "Invalid user_type"}), 400
+
     conn.commit()
 
     return jsonify({"msg": "User created successfully"}), 201
+
 
 
 @auth_blueprint.route('/protected', methods=['GET'])
