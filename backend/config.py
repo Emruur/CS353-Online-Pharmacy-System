ab08dfd700394e7f847d44cc5ddcb630f1032afa
tworule1 <saglamtugrul25@gmail.com>
2023-05-27 18:43:14 +0300

    bos

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 db_config = {
     "host": "localhost",
     "user": "root",
-    "password": "",  #change according to your settings
+    "password": "password",  #change according to your settings
     "database": "cs353db",
 }
