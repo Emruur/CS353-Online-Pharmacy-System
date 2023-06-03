90c51714a120ff960e36ff16820e1c2a382ef0ec
Emre Uğur <46279069+Emruur@users.noreply.github.com>
2023-06-03 14:55:23 +0300

    emöre

--- backend/create_table.sql
+++ backend/create_table.sql
@@ -154,16 +154,14 @@ CREATE TABLE Wallet (
     FOREIGN KEY (payment_id) REFERENCES PaymentMethod(payment_id)
 );
 CREATE TABLE Purchase (
-    purchase_id INTEGER PRIMARY KEY,
+    purchase_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     pharmacy_id INTEGER,
     date DATE,
     deduction Numeric(10, 2) NOT NULL,
     wallet_id INTEGER,
-    address_id INTEGER,
     user_id varchar(11),
     FOREIGN KEY (user_id) REFERENCES User(user_id),
     FOREIGN KEY (wallet_id) REFERENCES Wallet(wallet_id),
-    FOREIGN KEY (address_id) REFERENCES Address(address_id),
     FOREIGN KEY (pharmacy_id) REFERENCES Pharmacy(pharmacy_id)
 );
 CREATE TABLE PurchasedMedicine (
