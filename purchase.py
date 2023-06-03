3492126fac27f7557faa93b5302d057afd0d0738
Emre Uğur <46279069+Emruur@users.noreply.github.com>
2023-06-03 16:40:31 +0300

    purchase

--- purchase.py
+++ purchase.py
@@ -2,6 +2,8 @@ import mysql
 from flask import Blueprint, g, request, jsonify
 from config import db_config
 from datetime import datetime
+import json
+from collections import defaultdict
 
 from flask_jwt_extended import (
     jwt_required,
@@ -26,14 +28,14 @@ def close_conn(e):
         conn.close()
 
 
-@purchase_blueprint.route('/request', methods=['POST', 'GET'])
+@purchase_blueprint.route('/', methods=['POST', 'GET'])
 @jwt_required()
 def purchase():
     """
     prescription() can be used to create a new prescription object, or fetch all the prescriptions written for the logged
     in user. Requires authentication.
 
-    http://localhost:5000/prescription
+    http://localhost:5000/purchase
     post request is atomic
     post request data format:
     {
@@ -108,21 +110,11 @@ def purchase():
                 purchase_id = cursor.lastrowid
                 ## create purchased medicine objects
                 for med in med_list:
-                    
-                
+                    cursor.execute("""
+                        INSERT INTO PurchasedMedicine (purchase_id, purchase_count, med_id) 
+                        VALUES (?, ?, ?);           
+                    """,(purchase_id,med["quentity"],med["id"]))
 
-
-
-
-
-
-
-
-
-
-                
-            
-                
                 conn.commit()
                 return jsonify({"msg": "Purchase created successfully"}), 200
             except Exception as e:
@@ -135,16 +127,46 @@ def purchase():
             return jsonify({"msg": str(e)}), 405
 
     elif request.method == 'GET':
+        '''{
+        "purchase_id": 1,
+        "pharmacy_id": 1,
+        "date": "2023-06-03",
+        "deduction": 100.00,
+        "wallet_id": 2,
+        "user_id": "U001",
+        "med_id": [101, 102],
+        "purchase_count": [2, 3]
+    },
+        '''
+        
         try:
-            keys = ["id", "prescribed_by", "prescribed_to", "date", "type", "notes", "status"]
-            cursor.execute(
-                "select * from prescription where prescribed_to = %s",
-                (current_user,)
-            )
-            prescriptions = cursor.fetchall()
-            return [dict(zip(keys, row)) for row in prescriptions], 200
+            # Execute the query
+            cursor.execute("""
+                SELECT Purchase.*, PurchasedMedicine.med_id, PurchasedMedicine.purchase_count 
+                FROM Purchase
+                INNER JOIN PurchasedMedicine ON Purchase.purchase_id = PurchasedMedicine.purchase_id;
+            """)
+
+            # Fetch all rows from the last executed statement
+            rows = cursor.fetchall()
+
+            # Group rows by purchase_id
+            grouped = defaultdict(lambda: defaultdict(list))
+            for row in rows:
+                purchase_id = row.pop('purchase_id')
+                grouped[purchase_id]['purchase_id'] = purchase_id
+                for key, value in row.items():
+                    if key in ['med_id', 'purchase_count']:
+                        grouped[purchase_id][key].append(value)
+                    else:
+                        grouped[purchase_id][key] = value
+
+            # Convert to JSON
+            json_data = json.dumps(list(grouped.values()), default=str)  # Convert dates to string
+
+            return json_data
         except Exception as e:
             print(e)
-            return jsonify({"msg": "Couldn't get prescription"}), 500
+            return jsonify({"msg": "Couldn't get purchases"}), 500
 
 
