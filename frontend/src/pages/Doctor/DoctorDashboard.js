7b7fdadbd7a2d51ec527521147f7090a21ec25ed
BarisYildirim6 <byildirim2561@gmail.com>
2023-06-06 04:02:43 +0300

    Doctor dashboard fixed

--- frontend/src/pages/Doctor/DoctorDashboard.js
+++ frontend/src/pages/Doctor/DoctorDashboard.js
@@ -6,6 +6,7 @@ import axios from 'axios_config';
 const DoctorDashboard = () => {
 	
 	const [reqPrescriptions, setReqPrescriptions] = useState([]);
+    const [allPrescriptions, setAllPrescriptions] = useState([]);
 
 	const token = "Bearer " + sessionStorage.getItem("token");
 
@@ -28,6 +29,33 @@ const DoctorDashboard = () => {
                             }
                         }
                         setReqPrescriptions(arr);
+                        //console.log(arr)
+                    }
+                })
+                .catch((err) => {
+                    if (err && err.response) {
+                        console.log(err.response.data)
+                    }
+                })
+        }
+        const getAllPrescription = async () => {
+            await axios.get('/prescription/doctor', {
+                headers: {
+                    "Authorization": token
+                }
+            })
+                .then((res) => {
+                    if (res && res.data) {
+                        console.log(res.data)
+                        let arr = []
+                        for (let i = 0; i < res.data.length; i++) {
+                            if (arr[res.data[i].pres_id]) {
+                                arr[res.data[i].pres_id].push(res.data[i]);
+                            } else {
+                                arr[res.data[i].pres_id] = [res.data[i]];
+                            }
+                        }
+                        setAllPrescriptions(arr);
                         console.log(arr)
                     }
                 })
@@ -37,6 +65,7 @@ const DoctorDashboard = () => {
                     }
                 })
         }
+        getAllPrescription();
         getReqPrescription();
 	}, [])
 
@@ -52,6 +81,21 @@ const DoctorDashboard = () => {
                             </Typography>
                         </Box>
                         {reqPrescriptions.map((prescription, index) => (
+                            <Box key={index}>
+                                <Collapsable 
+                                    prescription={prescription}
+                                    //requestPrescription={requestPrescription}    
+                                />
+                            </Box>
+                        ))}
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                        <Box sx={{ my: 3 }}>
+                            <Typography color="textPrimary" variant="h4">
+                                All Prescriptions
+                            </Typography>
+                        </Box>
+                        {allPrescriptions.map((prescription, index) => (
                             <Box key={index}>
                                 <Collapsable 
                                     prescription={prescription}
